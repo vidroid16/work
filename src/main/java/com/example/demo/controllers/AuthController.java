b004de94ddd77c9ec26e017281cb075f97b55671
vidroid16 <shalyaandrey16@gmail.com>
2021-12-26 13:30:46 +0300

    commmit

--- src/main/java/com/example/demo/controllers/AuthController.java
+++ src/main/java/com/example/demo/controllers/AuthController.java
@@ -7,13 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -31,11 +29,11 @@ import java.util.Map;
         }
 
         @PostMapping("/login")
-        public ResponseEntity login(@RequestParam(value = "username") String username, @RequestParam(value = "password") String password) {
-            String req = accountService.login(username,password);
+        public ResponseEntity login(@RequestBody HashMap<String,String> account) {
+            String req = accountService.login(account.get("username"),account.get("password"));
             if (!req.equals("bad")) {
                 Map<Object, Object> response = new HashMap<>();
-                response.put("username", username);
+                response.put("username", account.get("username"));
                 response.put("token", req);
                 return ResponseEntity.ok(response);
             } else {
